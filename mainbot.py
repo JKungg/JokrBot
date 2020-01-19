cd5753f2e9a00d229fb57808ea5e2526ee3c1198
kungfu8 <jackdilkens@gmail.com>
2020-01-19 16:52:35 -0500

    Add files via upload

---
+++ mainbot.py
@@ -0,0 +1,34 @@
+import discord
+from discord.ext import commands
+import asyncio
+import os
+
+
+TOKEN = 'NjY4MjYzNTIwNzEzODM0NTI0.XiSO5Q.pk7w0WeMJldzb7kzl7E9B31cLeo'
+
+client = commands.Bot(command_prefix = '!')
+
+@client.event
+async def on_ready():
+    print('Logged in as')
+    print(client.user.name)
+    print(client.user.id)
+    print('------')
+
+
+@client.command()
+@commands.has_role('jokr')
+async def scrim(ctx, arg1, arg2):
+    await client.get_channel(615938985914662922).send(f"@everyone Scrim Scheduled for: **{arg1.capitalize()}, {arg2.capitalize()}**")
+
+
+@client.event
+async def on_message(message):
+    # we do not want the bot to reply to itself
+    if message.author == client.user:
+        return
+    if message.author.id == 372522812403089409:
+        await message.delete()
+
+
+client.run(TOKEN)

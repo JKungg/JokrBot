b70bda6dc00238ad5554ad7402945435154c1fe5
kungfu8 <jackdilkens@gmail.com>
2020-01-19 21:52:46 -0500

    Version 1.0 | .sreminds : .scrim : .clear

--- mainbot.py
+++ mainbot.py
@@ -5,8 +5,27 @@ import os
 
 token = os.environ.get('AuthToken')
 
-bot = commands.Bot(command_prefix = '!')
-client = discord.Client()
+client = commands.Bot(command_prefix = '.')
+
+
+@client.command()
+@commands.has_role('jokr')
+async def scrim(ctx, message, arg1, arg2):
+    await client.get_channel(615938985914662922).send(f"@everyone Scrim Scheduled for: **{arg1.capitalize()}, {arg2.capitalize()}**")
+    await ctx.message.delete()
+
+@client.command()
+@commands.has_role('admin')
+async def clear(ctx, amount=6):
+    await ctx.channel.purge(limit=6)
+    await ctx.channel.send("**Succesfully deleted last 5 messages :)**")
+
+
+@client.command()
+async def sremind(ctx, arg1, arg2):
+    await client.get_channel(631644755083657267).send(f"```Reminder you have a scrim in >>> {arg1} {arg2}```")
+    await ctx.message.delete()
+
 
 @client.event
 async def on_ready():
@@ -14,26 +33,5 @@ async def on_ready():
     print(client.user.name)
     print(client.user.id)
     print('------')
-
-
-@bot.command()
-@commands.has_role('jokr')
-async def scrim(ctx, arg1, arg2):
-    await client.get_channel(615938985914662922).send(f"everyone Scrim Scheduled for: **{arg1.capitalize()}, {arg2.capitalize()}**")
-
-
-@bot.command()
-async def sremind(ctx, arg1):
-    await client.get_channel(631644755083657267).send(f"Reminder you have a scrim in >>> {arg1}")
-
-
-@client.event
-async def on_message(message):
-    # we do not want the bot to reply to itself
-    if message.author == client.user:
-        return
-    #if message.author.id == 372522812403089409:
-        #await message.delete()
-
 
 client.run(token)

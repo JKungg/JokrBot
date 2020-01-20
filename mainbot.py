5d1fb671ddf0e4812b536c39dc38950f9cfdf4cd
kungfu8 <jackdilkens@gmail.com>
2020-01-19 19:51:13 -0500

    !!! NOT WORKING ATM !!! Added !sremind {arg1} commands are not working!

--- mainbot.py
+++ mainbot.py
@@ -5,7 +5,8 @@ import os
 
 token = os.environ.get('AuthToken')
 
-client = commands.Bot(command_prefix = '!')
+bot = commands.Bot(command_prefix = '!')
+client = discord.Client()
 
 @client.event
 async def on_ready():
@@ -15,10 +16,15 @@ async def on_ready():
     print('------')
 
 
-@client.command()
+@bot.command()
 @commands.has_role('jokr')
 async def scrim(ctx, arg1, arg2):
-    await client.get_channel(615938985914662922).send(f"@everyone Scrim Scheduled for: **{arg1.capitalize()}, {arg2.capitalize()}**")
+    await client.get_channel(615938985914662922).send(f"everyone Scrim Scheduled for: **{arg1.capitalize()}, {arg2.capitalize()}**")
+
+
+@bot.command()
+async def sremind(ctx, arg1):
+    await client.get_channel(631644755083657267).send(f"Reminder you have a scrim in >>> {arg1}")
 
 
 @client.event
@@ -26,8 +32,8 @@ async def on_message(message):
     # we do not want the bot to reply to itself
     if message.author == client.user:
         return
-    if message.author.id == 372522812403089409:
-        await message.delete()
+    #if message.author.id == 372522812403089409:
+        #await message.delete()
 
 
 client.run(token)

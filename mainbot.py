9248ae8b7e0a51746fbf95b5305687868205bb94
kungfu8 <jackdilkens@gmail.com>
2020-01-21 16:10:44 -0500

    Added Statistics for Rock Paper Scissors Games (.rpcstats)

--- mainbot.py
+++ mainbot.py
@@ -24,8 +24,8 @@ async def clear(ctx, amount=6):
 
 @client.command()
 async def cmds(ctx):
-    await ctx.channel.send("I currently have 3 Working Commands")
-    await ctx.channel.send("```.sremind (time) >>> Reminds the team the amount of time before a scrim!\n.scrim (day), (time) >>> Posts the scrim into the scrim schedule!\n.rpc (rock, paper or scissors) >>> Simple game of Rock Paper Scissors!\nAnd the last command is not for you!```")
+    await ctx.channel.send("I currently have a few Working Commands")
+    await ctx.channel.send("```.sremind (time) >>> Reminds the team the amount of time before a scrim!\n.scrim (day), (time) >>> Posts the scrim into the scrim schedule!\n.rpc (rock, paper or scissors) >>> Simple game of Rock Paper Scissors!\n.rpcstats >>> Shows the total amount of server wins, ties and losses for RPC Games.\n.ryan >> Shows a beautiful picture of ryan!\nAnd the last command is not for you!```")
 
 
 @client.command()
@@ -36,6 +36,7 @@ async def sremind(ctx, arg1, arg2):
 @client.command()
 async def rpc(ctx, int1):
     aiPick = random.randint(1,3)
+    result = 'None'
     if int1 == 'rock':
         await ctx.channel.send("Hmm what should I pick?")
         time.sleep(1)
@@ -65,7 +66,40 @@ async def rpc(ctx, int1):
             await ctx.channel.send("We both chose **scissors** it's a draw!")
     else:
         await ctx.channel.send("Error! 404")
+    if result == 'Win':
+        stats = open("rpcjokrstats.txt", "a")
+        stats.write("Win ")
+        stats.close()
+    elif result == 'Tie':
+        stats = open("rpcjokrstats.txt", "a")
+        stats.write("Tie ")
+        stats.close()
+    elif result == 'Loss':
+        stats = open("rpcjokrstats.txt", "a")
+        stats.write("Loss ")
+        stats.close()
+    else:
+        await ctx.channel.send("Error 405!")
 
+@client.command()
+async def rpcstats(ctx):
+    stats = open("rpcjokrstats.txt", "r")
+    rsl = stats.read()
+    win = rsl.count('Win ')
+    ties = rsl.count('Tie ')
+    loss = rsl.count('Loss ')
+    stats.close()
+    await ctx.channel.send("```Wins = " + str(win) + "\nDraws = " + str(ties) + "\nLosses = " + str(loss) + "```")
+
+
+@client.command()
+async def ryan(ctx):
+    await ctx.channel.send("https://static-cdn.jtvnw.net/jtv_user_pictures/f0f76f1e-0ba2-4f3b-927d-c6df70a8022d-profile_image-300x300.png")
+
+@client.command()
+async def trenty111222(ctx):
+    await ctx.channel.send("https://cdn2-img.pressreader.com/pressdisplay/docserver/getimage.aspx?regionKey=OMvzDX2twTLVQEtqCt%2BWaA%3D%3D")
+    await ctx.channel.send("Love you Trent <3")
 
 @client.event
 async def on_ready():

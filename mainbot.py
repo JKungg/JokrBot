90261538ff691f9dbe04cf137820faa2fdf31fa6
kungfu8 <jackdilkens@gmail.com>
2020-02-13 12:10:18 -0500

    Send Team Matches every Monday at 12:10(Hopefully it's posted by then

--- mainbot.py
+++ mainbot.py
@@ -1,11 +1,8 @@
-import discord
-from discord.ext import commands
-import asyncio
-import os
-import random
-import time
-import datetime
-import json
+import discord, asyncio, os, random, time, json, requests
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
+from datetime import datetime
+from discord.ext import commands, tasks
+from bs4 import BeautifulSoup
 
 token = os.environ.get('AuthToken')
 
@@ -34,6 +31,58 @@ async def sremind(ctx, arg1, arg2):
     await ctx.message.delete()
 
 
+# START MATCH GETTER
+
+def getTeams():
+
+    page = requests.get("https://vrmasterleague.com/EchoArena/Matches/")
+    soup = BeautifulSoup(page.content, 'html.parser')
+    matchNode = soup.find(id='MatchesScheduled_MatchesNode')
+    matchesHidden = matchNode.find_all(class_ = 'rows-hider-body rows-hider-hidden')
+    orangeCell = matchNode.find_all(class_ = 'home_team_cell')
+    blueCell = matchNode.find_all(class_ = 'away_team_cell')
+
+    global oteams
+    global bteams
+    # Get Teams in LIST \/ \/
+    bteams = list()
+    for teams in blueCell:
+        bteams.append(teams.get_text())
+
+    oteams = list()
+    for teams in orangeCell:
+        oteams.append(teams.get_text())
+
+    
+async def sendScrape():
+# 
+    await client.wait_until_ready()
+    matchchannel = client.get_channel(676243741811671050)
+
+    getTeams()
+# 
+    counter = 0
+    for team  in bteams:
+        if team == "Team Jokr":
+            orangeTeam = oteams[counter]
+            await matchchannel.send(f'@everyone Team Jokr vs {orangeTeam}')
+        counter += 1
+
+
+    counter = 0
+    for team in oteams:
+        if team == "Team Jokr":
+            blueTeam = bteams[counter]
+            await matchchannel.send(f'@everyone Team Jokr vs {blueTeam}')
+        counter += 1
+
+
+scheduler = AsyncIOScheduler()
+scheduler.add_job(sendScrape, 'cron', day_of_week=0, hour=12, minute=10)
+scheduler.start()
+
+# END OF MATCH GETTER
+
 ## General Use Commands.
 
 
@@ -49,81 +98,7 @@ async def clear(ctx, int1):
 @client.command()
 async def cmds(ctx):
     await ctx.channel.send("I currently have a few Working Commands")
-    await ctx.channel.send("```.sremind (time) >>> Reminds the team the amount of time before a scrim/game!\n.scrim (day) (time) >>> Posts the scrim into the scrim schedule!\n.setgame (day) (time) >>> For settings League Games!\n.rpc (rock, paper or scissors) >>> Simple game of Rock Paper Scissors!\n.rpcstats >>> Shows the total amount of server wins, ties and losses for RPC Games.\n.findword (word) >>> Little Dictionnary that uses json imports.\n.ryan >> Shows a beautiful picture of ryan!\n.clear (amount of lines) >>> Clears the amount of lines given!```")
-
-@client.command()
-async def rpc(ctx, int1):
-    aiPick = random.randint(1,3)
-    result = 'None'
-    if int1 == 'rock':
-        await ctx.channel.send("Hmm what should I pick?")
-        time.sleep(1)
-        if aiPick == 1:
-            await ctx.channel.send("Whoops, we both picked **Rock**!")
-            result = 'Tie'
-        elif aiPick == 2:
-            await ctx.channel.send("My **paper** beat your **rock**!")
-            result = 'Loss'
-        elif aiPick == 3:
-            await ctx.channel.send("Your **rock** destroyed my **scissors**!")
-            result = 'Win'
-    elif int1 == 'paper':
-        await ctx.channel.send("Hmm what should I pick?")
-        time.sleep(1)
-        if aiPick == 1:
-            await ctx.channel.send("Your **paper** beat my **rock**! :(")
-            result = 'Win'
-        elif aiPick == 2:
-            await ctx.channel.send("We both chose **paper** it's a tie!")
-            result = 'Tie'
-        elif aiPick == 3:
-            await ctx.channel.send("My **scissors** destroyed your **paper**!")
-            result = 'Loss'
-    elif int1 == 'scissors':
-        await ctx.channel.send("Hmm what should I pick?")
-        time.sleep(1)
-        if aiPick == 1:
-            await ctx.channel.send("My **Rock** beat your **scissors**")
-            result = 'Loss'
-        elif aiPick == 2:
-            await ctx.channel.send("Your **scissors** destroyed my **paper**!")
-            result = 'Win'
-        elif aiPick == 3:
-            await ctx.channel.send("We both chose **scissors** it's a draw!")
-            result = 'Tie'
-    else:
-        await ctx.channel.send("Error Code 1!")
-    with open("JokrBot\jokrstats.txt", 'a') as stats:
-        if result == 'Win':
-            stats.write("Win ")
-        elif result == 'Tie':
-            stats.write("Tie ")
-        elif result == 'Loss':
-            stats.write("Loss ")
-        else:
-            await ctx.channel.send("Error Code 2!")
-    stats.close()
-    
-@client.command()
-async def rpcstats(ctx):
-    with open("JokrBot\jokrstats.txt", "r") as stats:
-        rsl = stats.read()
-        win = rsl.count('Win ')
-        ties = rsl.count('Tie ')
-        loss = rsl.count('Loss ')
-        stats.close()
-    winper = win/(ties+loss+win)
-    await ctx.channel.send(f"```Wins =  {win}\nDraws =  {ties}\nLosses =  {loss}\nWin Percentage =  {winper:.1%}```")
-
-@client.command()
-async def findword(ctx, arg1):
-    wordsdata = json.load(open("/home/JokrBot/wordsforbot.json"))
-    arg1 = arg1.casefold()
-    if arg1 in wordsdata:
-        wordfound = wordsdata.get(arg1)
-        await ctx.channel.send(' '.join(wordfound))
-    else:
-        await ctx.channel.send("Word not found, double check the spelling of the word.")
+    await ctx.channel.send("```.sremind (time) >>> Reminds the team the amount of time before a scrim/game!\n.scrim (day) (time) >>> Posts the scrim into the scrim schedule!\n.setgame (day) (time) >>> For settings League Games!\n.rps (rock, paper or scissors) >>> Simple game of Rock Paper Scissors!\n.rpsstats >>> Shows the total amount of server wins, ties and losses for RPS Games.\n.numguess >>> Number Guessing Game.\n.ryan >> Shows a beautiful picture of ryan!\n.clear (amount of lines) >>> Clears the amount of lines given!```")
 
 @client.command()
 async def numguess(ctx):
@@ -163,15 +138,6 @@ async def willwewin(ctx):
         await ctx.channel.send("You'll probably win! That's a Big Surprise!")
     else:
         await ctx.channel.send("Error Code 3")
-
-@client.command()
-async def ryan(ctx):
-    await ctx.channel.send("https://static-cdn.jtvnw.net/jtv_user_pictures/f0f76f1e-0ba2-4f3b-927d-c6df70a8022d-profile_image-300x300.png")
-
-@client.command()
-async def trenty111222(ctx):
-    await ctx.channel.send("https://cdn2-img.pressreader.com/pressdisplay/docserver/getimage.aspx?regionKey=OMvzDX2twTLVQEtqCt%2BWaA%3D%3D")
-    await ctx.channel.send("Love you Trent <3")
 
 @client.event
 async def on_ready():
